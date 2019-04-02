940f1ee6debfd21dc3a9f93a5aee36f7df359bf6
Dimowner <dimmony@gmail.com>
2019-04-03 02:46:59 +0300

    upload images into Firebase storage, display images in the list.

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,55 @@
 import 'dart:async';
-
-import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:location/location.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(A());
+class A extends StatelessWidget{@override Widget build(BuildContext context){return MaterialApp(home:SW());}}
+class SW extends StatefulWidget{@override St createState()=>St();}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: StWg());
-  }
-}
-
-class StWg extends StatefulWidget {
-  @override
-  St createState() => new St();
-}
-
-class St extends State<StWg> {
+class St extends State<SW> {
   final controller = PageController(initialPage: 0);
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-
-  File _image;
-
+  static const LatLng center = const LatLng(45.521563, -122.677433);
   List<Note> items;
-  StreamSubscription<Event> _subsc;
-  final ref = FirebaseDatabase.instance.reference().child('notes');
+  StreamSubscription<Event> subsc;
+  final ref=FirebaseDatabase.instance.reference().child('r');
+  final StorageReference stor=FirebaseStorage.instance.ref();
+  TextEditingController tCntr;
 
-  TextEditingController _titleController;
-
-  @override
-  void initState() {
+  @override void initState() {
     super.initState();
-    items = new List();
-    _subsc = ref.onChildAdded.listen(_onNoteAdded);
-    _titleController = new TextEditingController();
+    items=List();
+    subsc=ref.onChildAdded.listen(_onNoteAdded);
+    tCntr=TextEditingController();
   }
 
   Future _getImg() async {
-    var userLocation = await new Location().getLocation();
+    var l = await Location().getLocation();
     var i = await ImagePicker.pickImage(source: ImageSource.camera);
     if (i != null) {
-      setState(() {
-        _image = i;
-      });
-      print("Location:" + userLocation["latitude"].toString() + " " +
-          userLocation["longitude"].toString());
-      _createNewNote();
+      _checkIn((await(await stor.child(Random().nextInt(100000).toString()+".jpeg").putFile(i,StorageMetadata(contentType:"image/jpeg")).onComplete).ref.getDownloadURL()),l["latitude"],l["longitude"]);
     }
   }
 
-  @override
-  void dispose() {
-    _subsc.cancel();
+  @override void dispose() {
+    subsc.cancel();
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
+  @override Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(body: PageView(controller: controller, scrollDirection: Axis.horizontal, children: <Widget>[_buildPage0(true), _buildList(context),],)));
   }
 
   Widget _buildPage0(bool left) {
     return Scaffold(
-        key: PageStorageKey<String>("page0"),
         body: _buildMap(),
-        floatingActionButton: new Stack(children: [
-          new Container(
-            margin: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        floatingActionButton:Stack(children: [
+          Container(
+            margin:EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
             child: RawMaterialButton(
               fillColor: Colors.yellow,
               splashColor: Colors.yellowAccent,
@@ -87,30 +64,27 @@ class St extends State<StWg> {
     );
   }
 
-  Widget _buildMap() {
-    return GoogleMap(onMapCreated: _onMapCreated, initialCameraPosition: CameraPosition(target: _center, zoom: 11.0,),);
-  }
+  Widget _buildMap() {return GoogleMap(onMapCreated:_onMapCreated,initialCameraPosition:CameraPosition(target:center,zoom:11.0));}
 
   void _onMapCreated(GoogleMapController controller) {
 //    _controller2.complete(controller);
   }
 
-  void _doAct() {
-    controller.animateToPage(1, duration: Duration(milliseconds: 250), curve: Curves.ease);
-  }
+  void _doAct() {controller.animateToPage(1,duration:Duration(milliseconds:250),curve:Curves.ease);}
 
   Widget _buildList(BuildContext context) {
     return Center(
-      child: ListView.builder(
-          itemCount: items.length,
-          padding: const EdgeInsets.all(15.0),
-          itemBuilder: (context, position) {
+      child:ListView.builder(
+          itemCount:items.length,
+//          padding:const EdgeInsets.all(16),
+          itemBuilder:(context, position) {
             return Column(
-              children: <Widget>[
-                Divider(height: 5.0),
+              children:<Widget>[
+                Divider(height: 12),
                 ListTile(
-                  title: Text('${items[position].title}', style: TextStyle(fontSize: 22.0, color: Colors.red,),),
-                  subtitle: Text('${items[position].description}', style: new TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic,),),
+                  leading:CircleAvatar(radius:32,backgroundImage:NetworkImage("${items[position].url}")),
+                  title:Text('${items[position].txt}',style:TextStyle(fontSize:24,color:Colors.red)),
+                  subtitle:Text('${items[position].lat}',style:TextStyle(fontSize:18,fontStyle:FontStyle.italic)),
                 ),
               ],
             );
@@ -120,34 +94,20 @@ class St extends State<StWg> {
 
   void _onNoteAdded(Event event) {
     setState(() {
-      items.add(new Note.fromSnapshot(event.snapshot));
+      items.add(Note.fromSnapshot(event.snapshot));
     });
   }
 
-  void _createNewNote() async {
-    Navigator.of(context).push(new MaterialPageRoute(builder:
-        (BuildContext context) => Scaffold(
-      appBar: AppBar(title: Text('Check-in')),
-      body: Container(
-        margin: EdgeInsets.all(16),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: _titleController,
-              decoration: InputDecoration(labelText: 'Title'),
-            ),
-            Padding(padding: new EdgeInsets.all(5.0)),
-            RaisedButton(
-              child: Text('Add'),
-              onPressed: () {
-                  ref.push().set({
-                    'title': _titleController.text,
-                    'description': "text"
-                  }).then((_) {
-                    Navigator.pop(context);
-                  });
-              },
-            ),
+  void _checkIn(String u, double lt, double ln)async{
+    Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context)=>Scaffold(
+      appBar:AppBar(title:Text('Check-in')),
+      body:Container(
+        margin:EdgeInsets.all(16),
+        child:Column(
+          children:<Widget>[
+            TextField(controller:tCntr,decoration:InputDecoration(labelText:'Title'),),
+            Padding(padding:EdgeInsets.all(8)),
+            RaisedButton(child:Text('Post'),onPressed:(){ref.push().set({'txt':tCntr.text,'url':u,'lat':lt,'lng':ln}).then((_){Navigator.pop(context);});}),
           ],
         ),
       ),
@@ -156,19 +116,8 @@ class St extends State<StWg> {
 }
 
 class Note {
-  String id,title,description;
-
-  Note(this.id, this.title, this.description);
-
-  Note.map(dynamic o) {
-    this.id = o['id'];
-    this.title = o['title'];
-    this.description = o['description'];
-  }
-
-  Note.fromSnapshot(DataSnapshot s) {
-    id = s.key;
-    title = s.value['title'];
-    description = s.value['description'];
-  }
+  String id,txt,url;double lat,lng;
+  Note(this.id,this.txt,this.url);
+  Note.map(dynamic o) {this.id=o['id'];this.txt=o['txt'];this.url=o['url'];this.lat=o['lat'];this.lng=o['lng'];}
+  Note.fromSnapshot(DataSnapshot s) {id=s.key;txt=s.value['txt'];lat=s.value['lat'];lng=s.value['lng'];url=s.value['url'];}
 }
