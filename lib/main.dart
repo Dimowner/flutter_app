757464c9daede307098f8364df2ff7d219c25c84
Dimowner <dimmony@gmail.com>
2019-04-05 00:43:31 +0300

    Put text input to map screen; Decrease code base.

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,59 @@
-import 'dart:async';
-import 'dart:math';
+import 'dart:async';import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'dart:io' as Io;
-import 'package:image/image.dart' as Im;
+import 'dart:io' as Io;import 'package:image/image.dart' as Im;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:location/location.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 void main()=>runApp(A());
-class A extends StatelessWidget{@override Widget build(BuildContext context){return MaterialApp(home:SW());}}
-class SW extends StatefulWidget{@override St createState()=>St();}
-class St extends State<SW> {
-  List<L> it;StreamSubscription<Event> sub;
-  final ref=FirebaseDatabase.instance.reference().child('r');
-  final sr=FirebaseStorage.instance.ref();
-  TextEditingController t;double lt,ln; int id=1;
-  Map<MarkerId,Marker>m=<MarkerId,Marker>{};
-  GoogleMapController ct;
+class A extends StatelessWidget{@override Widget build(BuildContext context){return MaterialApp(home:W());}}
+class W extends StatefulWidget{@override S createState()=>S();}
+class S extends State<W> {
 
-  @override void initState() {super.initState();it=List();sub=ref.onChildAdded.listen(onAdd);t=TextEditingController();}
-  Future img() async {
-    var f=await ImagePicker.pickImage(source:ImageSource.camera);
-    if(f!=null){Im.Image im=Im.decodeImage(Io.File(f.path).readAsBytesSync());Im.Image th=Im.copyResize(im,600);Io.File(f.path)..writeAsBytesSync(Im.encodePng(th));
-      cin((await(await sr.child(Random().nextInt(10000).toString()+".jpeg").putFile(f,StorageMetadata(contentType:"image/jpeg")).onComplete).ref.getDownloadURL()),lt,ln);}
-  }
-  Future loc() async{
-    var l=await Location().getLocation();lt=l["latitude"];ln=l["longitude"];
-    setState((){m.clear();m[MarkerId("id$id")]=Marker(markerId:MarkerId("id$id"),position:LatLng(lt,ln));});
-    id++;ct.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(zoom:12,target:LatLng(lt,ln))));
-  }
-  @override void dispose(){sub.cancel();super.dispose();}
-  @override Widget build(BuildContext context){return MaterialApp(home:Scaffold(appBar:AppBar(title:Text('Check-in')),body:list(context)));}
-  Widget map(int p){
-    var ll=LatLng(50,30);if (p>=0){ll=LatLng(it[p].lat,it[p].lng);m[MarkerId("id$id")]=Marker(markerId:MarkerId("id$id"),position:ll);}
-    id++;return GoogleMap(scrollGesturesEnabled:true,zoomGesturesEnabled:true,onMapCreated:(c){ct=c;},markers:Set<Marker>.of(m.values),initialCameraPosition:CameraPosition(target:ll,zoom:12));
-  }
-  Widget list(BuildContext context){
-    return Scaffold(backgroundColor:Colors.blue[600],body:ListView.builder(itemCount:it.length,itemBuilder:(context,pos) {
-      return Stack(alignment:AlignmentDirectional.bottomStart,children:<Widget>[
-        Padding(padding:EdgeInsets.all(12),child:Stack(alignment:AlignmentDirectional.centerStart,children:<Widget>[
-        Card(color: Colors.lightBlue,shape:StadiumBorder(),child:Container(width:double.infinity,child:Padding(padding:EdgeInsets.only(left:80,right:16,top:16,bottom:16),child:Text('${it[pos].txt}',
-          style:TextStyle(fontSize:20,color:Colors.white))))),
-        GestureDetector(child:CircleAvatar(radius:36,backgroundImage:NetworkImage("${it[pos].url}")),
-          onTap:(){Navigator.push(context,MaterialPageRoute(builder:(_){
-            return Scaffold(appBar:AppBar(title:Text("${it[pos].txt}")),body:Image.network("${it[pos].url}",
-            fit:BoxFit.cover,height:double.infinity,width:double.infinity));}));})]))]);}),
-      floatingActionButton:FloatingActionButton(backgroundColor:Colors.green,child:Icon(Icons.location_on),onPressed:map2)
-    );
-  }
-  void onAdd(Event e){setState((){it.insert(0,L.fromSnapshot(e.snapshot));});}
-  void map2(){
-    Navigator.push(context, MaterialPageRoute(builder:(_){return Scaffold(
-     appBar:AppBar(automaticallyImplyLeading:false,title:Text('Your location')),body:map(-1),floatingActionButton:Row(mainAxisSize:MainAxisSize.min,children:[
-       Container(margin:EdgeInsets.symmetric(horizontal:24),child:RawMaterialButton(fillColor:Colors.yellow,splashColor:Colors.yellowAccent,
-         child:Padding(padding:EdgeInsets.all(12),child: Row(children:<Widget>[Icon(Icons.arrow_back),Text("Back")])),
-         onPressed:(){Navigator.pop(context);},shape:StadiumBorder())),
-       FloatingActionButton(child:Icon(Icons.location_on),onPressed:loc),
-       Container(margin:EdgeInsets.symmetric(horizontal:24),child:RawMaterialButton(fillColor:Colors.yellow,splashColor:Colors.yellowAccent,
-         child:Padding(padding:EdgeInsets.all(12),child:Row(children:<Widget>[Text("Next"),Icon(Icons.arrow_forward)])),
-         onPressed:img,shape:StadiumBorder())),
-     ]),floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat);}));
-  }
-  void cin(String u,double lt,double ln)async{
-    Navigator.of(context).push(MaterialPageRoute(builder:(BuildContext context)=>Scaffold(appBar:AppBar(title:Text('Check-in')),
-      body:Container(margin:EdgeInsets.all(16),child:Column(children:<Widget>[TextField(controller:t,decoration:InputDecoration(labelText:'Title'),),Padding(padding:EdgeInsets.all(8)),
-      RaisedButton(child:Text('Post'),onPressed:(){ref.push().set({'txt':t.text,'url':u,'lat':lt,'lng':ln}).then((_){Navigator.pop(context);});})])))));
-  }
+List<L> it;StreamSubscription<Event>sb;
+final r=FirebaseDatabase.instance.reference().child('r');
+final sr=FirebaseStorage.instance.ref();
+TextEditingController t;
+double lt,ln; int d=1;
+Map<MarkerId,Marker>m=<MarkerId,Marker>{};GoogleMapController ct;
+
+@override void initState() {super.initState();it=List();sb=r.onChildAdded.listen((e){setState((){it.insert(0,L.fromSnapshot(e.snapshot));});});t=TextEditingController();}
+Future img() async {
+  var f=await ImagePicker.pickImage(source:ImageSource.camera);
+  if(f!=null){Im.Image im=Im.decodeImage(Io.File(f.path).readAsBytesSync());Im.Image th=Im.copyResize(im,900);Io.File(f.path)..writeAsBytesSync(Im.encodePng(th));}
+  var u=await(await sr.child(Random().nextInt(10000).toString()+".jpeg").putFile(f,StorageMetadata(contentType:"image/jpeg")).onComplete).ref.getDownloadURL();
+  r.push().set({'txt':t.text,'url':u,'lat':lt,'lng':ln});Navigator.pop(context);
+}
+Future loc() async{
+  var l=await Location().getLocation();lt=l["latitude"];ln=l["longitude"];
+  setState((){m.clear();m[MarkerId("id$d")]=Marker(markerId:MarkerId("id$d"),position:LatLng(lt,ln));});
+  d++;ct.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(zoom:12,target:LatLng(lt,ln))));
+}
+@override void dispose(){sb.cancel();super.dispose();}
+@override Widget build(BuildContext c){return MaterialApp(home:Scaffold(appBar:AppBar(title:Text('Check-in')),body:ls(c)));}
+Widget map(int p){
+  var ll=LatLng(37.422,-122.08);if(p>=0){ll=LatLng(it[p].lat,it[p].lng);m[MarkerId("id$d")]=Marker(markerId:MarkerId("id$d"),position:ll);}
+  d++;return GoogleMap(scrollGesturesEnabled:true,zoomGesturesEnabled:true,onMapCreated:(c){ct=c;},markers:Set<Marker>.of(m.values),initialCameraPosition:CameraPosition(target:ll,zoom:12));
+}
+Widget ls(BuildContext c){
+  return Scaffold(backgroundColor:Colors.blue[600],body:ListView.builder(itemCount:it.length,itemBuilder:(c,pos){
+  return Stack(alignment:AlignmentDirectional.bottomStart,children:<Widget>[Padding(padding:EdgeInsets.all(12),child:Stack(alignment:AlignmentDirectional.centerStart,children:<Widget>[
+  GestureDetector( onTap:(){Navigator.push(c,MaterialPageRoute(builder:(_){return map(pos);}));},child:Card(color: Colors.lightBlue,shape:StadiumBorder(),child:Container(width:double.infinity,child:Padding(padding:EdgeInsets.only(left:80,right:16,top:16,bottom:16),child:Text('${it[pos].txt}', style:TextStyle(fontSize:20,color:Colors.white)))))),
+  GestureDetector(child:CircleAvatar(radius:36,backgroundImage:NetworkImage("${it[pos].url}")),onTap:(){Navigator.push(c,MaterialPageRoute(builder:(_){
+  return Scaffold(appBar:AppBar(title:Text("${it[pos].txt}")),body:Image.network("${it[pos].url}",fit:BoxFit.cover,height:double.infinity,width:double.infinity));}));})]))]);}),
+  floatingActionButton:FloatingActionButton(backgroundColor:Colors.green,child:Icon(Icons.location_on),onPressed:nw));
+}
+void nw(){
+  Navigator.push(context, MaterialPageRoute(builder:(_){return Scaffold(
+  appBar:AppBar(automaticallyImplyLeading:false,title:Text('Check-in')),body:Stack(alignment:AlignmentDirectional.bottomStart,children:<Widget>[map(-1),
+  Container(height:150,decoration:BoxDecoration(color:Colors.grey[200]),child:Padding(padding:EdgeInsets.only(left:16,right:16),child:TextField(controller:t,decoration:InputDecoration(labelText:'Impression'))))]),floatingActionButton:Row(mainAxisSize:MainAxisSize.min,children:[
+  Container(margin:EdgeInsets.symmetric(horizontal:24),child:RawMaterialButton(fillColor:Colors.yellow,splashColor:Colors.yellow,child:Padding(padding:EdgeInsets.all(12),child:Row(children:<Widget>[Icon(Icons.arrow_back),Text("Back")])),onPressed:(){Navigator.pop(context);},shape:StadiumBorder())),
+  FloatingActionButton(child:Icon(Icons.location_on),onPressed:loc),
+  Container(margin:EdgeInsets.symmetric(horizontal:24),child:RawMaterialButton(fillColor:Colors.yellow,splashColor:Colors.yellow,child:Padding(padding:EdgeInsets.all(12),child:Row(children:<Widget>[Text("Next"),Icon(Icons.arrow_forward)])), onPressed:img,shape:StadiumBorder())),
+  ]),floatingActionButtonLocation:FloatingActionButtonLocation.centerFloat);}));
+}
 }
 class L{
   String id,txt,url;double lat,lng;
